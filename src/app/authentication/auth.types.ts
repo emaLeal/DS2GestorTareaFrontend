66b80e49eb03d0d5db923ad6cb59976da2da8bdd
kaniela04 <karenguzmanhernandez44@gmail.com>
2025-09-01 16:03:40 -0500

    creacion del componente de cambio de contraseña con validadores de campo y vista

--- src/app/authentication/auth.types.ts
+++ src/app/authentication/auth.types.ts
@@ -7,11 +7,6 @@ export interface User {
     name: string,
     last_name: string,
     document_id: string,
-    address: string,
-    phone: string,
-    cell_phone: string,
-    birth_date: string,
-    gender: string,
     identification_type: string,
     email: string,
     password: string
