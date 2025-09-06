46e2429f5762e17d74348599d0ce7d5b4a7e52bd
kaniela04 <karenguzmanhernandez44@gmail.com>
2025-09-06 15:23:40 -0500

    creacion de perfil usuario

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+
+interface User {
+  name: string ; 
+  last_name: string;
+  email: string ; 
+  identification_type: string; 
+  id: number;
+  departaments: string;
+  rol: string
+}
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink, RouterModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+   constructor(private router: Router) {}
+  user!: User;
+
+  ngOnInit(): void {
+    this.loadMockUser();
+  }
+
+  // simulacion de datos de usuario 
+  loadMockUser(): void {
+    this.user= {
+        name: "karen daniela", 
+        last_name: "guzman h", 
+        email: "karenh99@gamil.com", 
+        identification_type: "Cedula", 
+        id: 1145667890, 
+        departaments: "tecnologia", 
+        rol: "usuario" 
+      };
+  }
 
 }
