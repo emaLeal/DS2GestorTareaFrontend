d5528d73828b5ebe6696d0c45176f8b4b70aefde
emaLeal <emanuel.leal@correounivalle.edu.co>
2025-09-06 17:28:26 -0500

    Modulo de Registro Exitoso y Pendiente Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,25 +14,26 @@ import { ResetPasswordComponent } from './authentication/reset-password/reset-pa
 
 export const routes: Routes = [
 
-{ path: '', redirectTo: 'login', pathMatch: 'full' }, // carga login de entrada
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // carga login de entrada
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'change-password', component: ChangePasswordComponent }, // ruta para validar email pal cambio de contraseña 
-  { path: 'reset-password', component: ResetPasswordComponent}, // ruta para el cambio de contraseña
-  { path: 'dashboard', component: DashboardComponent , // Ruta protegida, solo accesible si el usuario está autenticado
- //luego se agrega canActivate:[Autenticated] para direccionar la ruta segun el rol del usuario 
- children : [ 
-  {path: 'profile', component: ProfileComponent}, // ruta para el componente perfil de usuario
-  {path: 'home-board', component: HomeBoardComponent}, // Ruta para el componente Dashboard
-  {path: 'taskflow', component: TaskflowComponent}, // Ruta para el componente Taskflow
-  {path: 'edit-task', component: EditTaskComponent}, // Ruta para el componente EditTask
-  {path: 'help', component: HelpComponent}, //Ruta para el componente de ayuda 
-  {path: '**', redirectTo: 'home-board' }, // ruta por defecto del dashboar
-   
-  ]
+  { path: 'change-password', component: ChangePasswordComponent }, // ruta para cambiar contraseña
+
+  {
+    path: 'dashboard', component: DashboardComponent, // Ruta protegida, solo accesible si el usuario está autenticado
+    //luego se agrega canActivate:[Autenticated] para direccionar la ruta segun el rol del usuario 
+    children: [
+      { path: 'profile', component: ProfileComponent }, // ruta para el componente perfil de usuario
+      { path: 'home-board', component: HomeBoardComponent }, // Ruta para el componente Dashboard
+      { path: 'taskflow', component: TaskflowComponent }, // Ruta para el componente Taskflow
+      { path: 'edit-task', component: EditTaskComponent }, // Ruta para el componente EditTask
+      { path: 'help', component: HelpComponent }, //Ruta para el componente de ayuda 
+      { path: '**', redirectTo: 'home-board' }, // ruta por defecto del dashboar
+
+    ]
   },
 
-  
+
   { path: '**', redirectTo: 'login' },//  atrapa rutas inválidas
- 
+
 ];
